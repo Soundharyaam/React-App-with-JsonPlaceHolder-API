c5cdd129b07e5a1442ab512f21ccdccd79964291
Soundharyaam <soundharyabharatiya@gmail.com>
2019-06-09 18:43:14 +0530

    Updates

---
+++ precache-manifest.5dd09ba5a9b9c9e125912bbdb706fd68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "770a95391234e3d8bf19",
+    "url": "/React-App-with-JsonPlaceHolder-API/static/js/runtime~main.770a9539.js"
+  },
+  {
+    "revision": "b01e6f634e3e9b8e9844",
+    "url": "/React-App-with-JsonPlaceHolder-API/static/js/main.b01e6f63.chunk.js"
+  },
+  {
+    "revision": "2ce80083061949cf9398",
+    "url": "/React-App-with-JsonPlaceHolder-API/static/js/2.2ce80083.chunk.js"
+  },
+  {
+    "revision": "b01e6f634e3e9b8e9844",
+    "url": "/React-App-with-JsonPlaceHolder-API/static/css/main.94f1a470.chunk.css"
+  },
+  {
+    "revision": "9b6c01edc6491bcea50896413e0ab18e",
+    "url": "/React-App-with-JsonPlaceHolder-API/index.html"
+  }
+];
